79891bbb9bf097e285c74608eb9641398b939098
Huy Hoang <huyhoang160593@yahoo.com>
2023-04-08 23:50:34 +0700

    done 3-8

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
-
-app.use(morgan("tiny"));
-
 app.use(express.json());
+
+morgan.token('body', (req, res) => req.body ? JSON.stringify(req.body) : '-')
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
 
 let persons = [
   {
