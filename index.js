793f37e353ecbd7680a7001a8538db67aa1b7f46
Huy Hoang <huyhoang160593@yahoo.com>
2023-04-09 10:30:09 +0700

    change to use the env PORT, done 3-10

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.get("/info", (_, response) => {
   response.end(new Date().toString());
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
